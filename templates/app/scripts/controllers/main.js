2661442a0c89193afa41ef95fc45127edb42ba6b
Matt Condon <MattCMultimedia@gmail.com>
2014-02-09 21:01:24 -0600

    added no-results text

--- templates/app/scripts/controllers/main.js
+++ templates/app/scripts/controllers/main.js
@@ -4,16 +4,6 @@ angular.module('BossBossApp')
 .controller('MainCtrl', function ($scope, DefaultClasses, $debounce, Search, _, localStorageService) {
     $scope.searchResults = DefaultClasses.get();
     $scope.selectedCourses = localStorageService.get('selectedCourses') || [];
-    $scope.loading = false;
-
-    $scope.$watch('searchResults', function() {
-        // $scope.loading = $scope.searchResults.length === 0;
-        $scope.loading = true;
-        var a = window.setInterval(function(){
-            $scope.loading = false;
-            clearInterval(a);
-        }, 1000);
-    });
 
     $scope.$watch('selectedCourses', function() {
         localStorageService.add('selectedCourses', $scope.selectedCourses);
@@ -22,7 +12,15 @@ angular.module('BossBossApp')
     $scope.$watch('searchText', $debounce(function() {
         $scope.searchResults = [];
         if ($scope.searchText !== '') {
-            $scope.searchResults = Search.get({q:$scope.searchText});
+            $scope.searchResults = Search.get({q:$scope.searchText}, function() {
+                if ($scope.searchResults.length === 0) {
+                    $scope.noResults = true;
+                    $scope.loading = false;
+                }
+            }, function(){
+                // OH NOES
+
+            });
         }
     }, 400), true);
 
