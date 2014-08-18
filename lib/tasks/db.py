464f49ee90f98dcbc624ca678c16540c099ccdba
Matt Condon <MattCMultimedia@gmail.com>
2014-08-17 21:57:01 -0500

    switch to peewee cause I don't have time to elarn sqlalchemy

--- lib/tasks/db.py
+++ lib/tasks/db.py
@@ -2,3 +2,4 @@ from sqlalchemy import create_engine
 import info
 
 engine = create_engine('mysql://%s:%s@%s/%s' % (info.db_user, info.db_pass, info.db_host, info.db_name), echo=True)
+conn = engine.connect()
